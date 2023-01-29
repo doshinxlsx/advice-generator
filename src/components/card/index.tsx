5b8dbcda1b5b515fd9d3141bf9b82f2ad823f9c7
Dominik Orincsai <o.dominik2107@gmail.com>
2023-01-29 05:00:28 +0100

    feat(*): added interface to API data and made the app done

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,21 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ButtonIcon from './components/buttonIcon';
 import Divider from './components/divider';
+import axios from 'axios';
 import { AdviceNumber, DiceButton, CardContainer, Message, DividerContainer } from './styles';
+import { IData } from '../../types';
 
 const Card = () => {
+  const [data, setData] = useState<IData>({
+    id: 1,
+    advice: '',
+  });
+
+  const generateAdvice = async () => {
+    try {
+      const res = await axios.get('https://api.adviceslip.com/advice');
+      setData(res.data.slip);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    generateAdvice();
+  }, []);
+
+  const handleClick = () => {
+    generateAdvice();
+  };
+
   return (
     <CardContainer>
-      <AdviceNumber>Advice #117</AdviceNumber>
-      <Message>
-        "It is easy to sit up and take notice, what's difficult is getting up and taking action."
-      </Message>
+      <AdviceNumber>Advice #{data.id}</AdviceNumber>
+      <Message>{data.advice}</Message>
 
       <DividerContainer>
         <Divider />
       </DividerContainer>
 
-      <DiceButton>
+      <DiceButton onClick={handleClick}>
         <ButtonIcon />
       </DiceButton>
     </CardContainer>
